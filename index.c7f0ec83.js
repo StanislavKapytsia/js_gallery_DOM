f51553b3c97ed12b358dca00891857e4a8d62652
Stas <staskapica93@gmail.com>
2024-09-02 21:07:59 +0200

    make build

---
+++ index.c7f0ec83.js
@@ -0,0 +1,2 @@
+document.getElementById("thumbs").addEventListener("click",function(e){e.preventDefault();var t=e.target.closest("a");t&&t.contains(e.target)&&(document.getElementById("largeImg").src=t.href)});
+//# sourceMappingURL=index.c7f0ec83.js.map
